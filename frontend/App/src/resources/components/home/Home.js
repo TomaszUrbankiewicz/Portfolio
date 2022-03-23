ff45b48b17a4e946a55fa262bd1b31fac82f049a
TomaszUrbankiewicz <tomasz.urbankiewicz@gmail.com>
2022-03-23 19:42:57 +0100

    adding animations for language selection

--- frontend/App/src/resources/components/home/Home.js
+++ frontend/App/src/resources/components/home/Home.js
@@ -2,10 +2,15 @@
 import React  from 'react';
 import Language_selection_to_start from "../unrelated/Language_selection_to_start "
 
-const Home  = () => {
+const Home  = (props) => {
+    
     return(
         <main id="main_page" className="conatainer_main">
-            <Language_selection_to_start/>
+            <Language_selection_to_start
+                change_languages={props.change_languages}
+            />
+            <h1>{props.language.h1}</h1>
+            <p>{props.language.hp}</p> 
         </main>
     );
 };
