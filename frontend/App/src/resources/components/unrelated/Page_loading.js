fc197e1fafb2f7555a425db20bce6ee02b08e834
TomaszUrbankiewicz <tomasz.urbankiewicz@gmail.com>
2022-03-28 21:15:06 +0200

    adding a countdown to page loading

--- frontend/App/src/resources/components/unrelated/Page_loading.js
+++ frontend/App/src/resources/components/unrelated/Page_loading.js
@@ -8,24 +8,25 @@ const Page_loading  = (props) => {
             setAnimation_number(Math.floor(Math.random() * (4 - 1)) + 1); //losuję numer animacji
     },[])
     
-    
-    return(
-        <section id='page_loading' className='container_page_loading'>
-            <div className='container_page_loading__circle'></div>
-            <div className='container_page_loading__text'>
-                <span className='text circle_text'>Leading...</span>
-            </div>
-        </section>
-    );
-    // return(
-    //     <section id='page_loading' className='container_page_loading'>
-    //         <div className='container_page_loading__cup'></div>
-    //         <div className='container_page_loading__text'>
-    //             <span className='text cup_text'>Leading...</span>
-    //         </div>
-    //     </section>
-    // );
-
-    
+    if(animation_number==1){
+        return(
+            <section id='page_loading' className={(props.page_loading)?'container_page_loading': null}>
+                <div className='container_page_loading__circle'></div>
+                <div className='container_page_loading__text'>
+                    <span className='text circle_text'>Leading...</span>
+                </div>
+            </section>
+        );
+    }
+    else if(animation_number==2){
+        return(
+            <section id='page_loading' className={(props.page_loading)?'container_page_loading': null}>
+                <div className='container_page_loading__cup'></div>
+                <div className='container_page_loading__text'>
+                    <span className='text cup_text'>Leading...</span>
+                </div>
+            </section>
+        );
+    }
 };
 export default Page_loading ;
