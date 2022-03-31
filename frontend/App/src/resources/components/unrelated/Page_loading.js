33fe3e7e845fe8ddbd90bb3b7c3c9004771e7292
TomaszUrbankiewicz <tomasz.urbankiewicz@gmail.com>
2022-03-31 20:06:13 +0200

    adding 3 animations and transition functionality

--- frontend/App/src/resources/components/unrelated/Page_loading.js
+++ frontend/App/src/resources/components/unrelated/Page_loading.js
@@ -2,15 +2,15 @@
 import React, { useEffect, useState } from 'react';
 
 const Page_loading  = (props) => {
-    const [animation_number, setAnimation_number] = useState(1)// zmienna do wyśfietlania lubnie flag na sronie
+    const [animation_number, setAnimation_number] = useState(3)// zmienna do wyśfietlania lubnie flag na sronie
 
-    // useEffect(() =>{
-    //         setAnimation_number(Math.floor(Math.random() * (4 - 1)) + 1); //losuję numer animacji
-    // },[])
+    useEffect(() =>{
+            setAnimation_number(Math.floor(Math.random() * (4 - 1)) + 1); //losuję numer animacji
+    },[])
     
     if(animation_number==1){
         return(
-            <section id='page_loading' className={(props.page_loading)?'container_page_loading': null}>
+            <section id='page_loading' className={(props.page_loading) ? "container_page_loading" : "container_page_loading container_page_loading-invisible"}>
                 <div className='container_page_loading__circle'>
                     <div className='container_page_loading__circle__small'></div>
                     <div className='container_page_loading__circle__tiny'></div>
@@ -23,10 +23,20 @@ const Page_loading  = (props) => {
     }
     else if(animation_number==2){
         return(
-            <section id='page_loading' className={(props.page_loading)?'container_page_loading': null}>
+            <section id='page_loading' className={(props.page_loading) ? "container_page_loading" : "container_page_loading container_page_loading-invisible"}>
                 <div className='container_page_loading__cup'></div>
                 <div className='container_page_loading__text'>
                     <span className='text cup_text'>Loading...</span>
+                </div>
+            </section>
+        );
+    }
+    else if(animation_number==3){
+        return(
+            <section id='page_loading' className={(props.page_loading) ? "container_page_loading" : "container_page_loading container_page_loading-invisible"}>
+                <div className='container_page_loading__loading_bar'></div>
+                <div className='container_page_loading__text'>
+                    <span className='text loading_bar_text'>Loading...</span>
                 </div>
             </section>
         );
