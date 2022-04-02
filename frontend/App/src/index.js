1b2f4cd8ba081d4198f5dd50079cc6f616eb040b
TomaszUrbankiewicz <tomasz.urbankiewicz@gmail.com>
2022-04-02 20:24:14 +0200

    add button for language selection

--- frontend/App/src/index.js
+++ frontend/App/src/index.js
@@ -12,6 +12,7 @@ const App = () => {
     const [language, setLanguage] = useState(pl)//zmienna odpowiadająca za pobieranie textów z dsanego pliku
     const [page_loading, setPage_loading] = useState(true) //czy ma sie pokazywac animacja ladowania strony
     const [show_flags, setShow_flags] = useState(true)// zmienna do wyśfietlania lubnie flag na sronie
+    const [active_button, setActive_button] = useState ("pl")// aktywny button jezyka
 
     useEffect(() =>{ //licze czas do wylaczenia animacji ladowania
         setTimeout(()=>{
@@ -27,33 +28,40 @@ const App = () => {
         if(e.target.id=="pl"){
             setLanguage(pl)
             window.localStorage.setItem("my_local",JSON.stringify({field_1:"pl"}))
+            setActive_button("pl")
         }
         else if(e.target.id=="en"){
             setLanguage(en)
             window.localStorage.setItem("my_local",JSON.stringify({field_1:"en"}))
+            setActive_button("en")
         }
         else if(e.target.id=="ru"){
             setLanguage(ru)
             window.localStorage.setItem("my_local",JSON.stringify({field_1:"ru"}))
+            setActive_button("ru")
         }
     }
     
-    useEffect(() =>{ // zapisanie do lokalnej pamięci języka 
+    useEffect(() =>{ // zapisanie z lokalnej pamięci języka 
         if(JSON.parse(window.localStorage.getItem('my_local'))!=null){
             if((JSON.parse(window.localStorage.getItem('my_local')).field_1)=="pl"){
                 setLanguage(pl);
+                setActive_button("pl")
             }
             else if((JSON.parse(window.localStorage.getItem('my_local')).field_1)=="en"){
                 setLanguage(en);
+                setActive_button("en")
             }
             else if((JSON.parse(window.localStorage.getItem('my_local')).field_1)=="ru"){
                 setLanguage(ru);
+                setActive_button("ru")
             }
             setShow_flags(false)
         }
         else{
             setLanguage(language);
             change_page_title(language)
+            setActive_button("pl")
         }
     },[])
 
@@ -64,13 +72,14 @@ const App = () => {
     if(show_flags){
         return(
             <>
-                <Page_loading page_loading={page_loading} /> 
+                {/* <Page_loading page_loading={page_loading} />  */}
                 <Language_selection_to_start
                     change_languages={change_languages}
                 />
                 <Home
                     change_languages={change_languages}
                     language={language}
+                    active_button={active_button}
                 />
             </>
             )
@@ -78,10 +87,11 @@ const App = () => {
         else{
             return(
                 <>
-                    <Page_loading page_loading={page_loading} /> 
+                    {/* <Page_loading page_loading={page_loading} />  */}
                     <Home
                         change_languages={change_languages}
                         language={language}
+                        active_button={active_button}
                     />
                 </>
             );
--- frontend/App/src/resources/components/home/Home.js
+++ frontend/App/src/resources/components/home/Home.js
@@ -1,10 +1,15 @@
 //komponent  strony startowej
 import React, { useEffect, useState } from 'react';
+import Language_buttons from "../unrelated/Language_buttons"
 
 const Home  = (props) => {
 
         return(
             <main id="main_page" className="conatainer_main">
+                <Language_buttons
+                    change_languages={props.change_languages}
+                    active_button={props.active_button}
+                />
                 <h1>{props.language.h1}</h1>
                 <p>{props.language.hp}</p> 
             </main>
